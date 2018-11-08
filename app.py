c03f07526fc37ccbe8b2f67f279239dd8028257d
IgorG94 <igorgoldstein94@gmail.com>
2018-11-08 17:27:35 -0200

    edit app.py in order to accept POST requests with audio files

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
+import os
 import audioTrainTest as aT
 
+UPLOAD_FOLDER = './'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './'
 
 audio = "training/audio1.wav"
 
 
 @app.route('/detect', methods=['GET', 'POST'])
-def detection(audio=audio):
-    result = aT.fileRegression(audio, "training/", "svm")
-    return jsonify(dict(zip(result[1], result[0])))
+def detection():
+    if request.method == 'POST':
+        f = dict(request.files)[''][0]
+        filename = secure_filename(f.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        f.save(filepath)
+        result = aT.fileRegression(filepath, "training/", "svm")
+        return jsonify(dict(zip(result[1], result[0])))
 
 
 if __name__ == "__main__":
