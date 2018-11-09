cd5b1099499f6188576a5cfb5aa9e55daae75d98
IgorG94 <igorgoldstein94@gmail.com>
2018-11-08 23:34:33 -0200

    edit route to accept 3gp files and convert them to wav

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
 import os
+import subprocess
 import audioTrainTest as aT
 
 UPLOAD_FOLDER = './'
@@ -14,11 +15,17 @@ audio = "training/audio1.wav"
 @app.route('/detect', methods=['GET', 'POST'])
 def detection():
     if request.method == 'POST':
-        f = dict(request.files)[''][0]
+        f = dict(request.files)['file'][0]
         filename = secure_filename(f.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         f.save(filepath)
-        result = aT.fileRegression(filepath, "training/", "svm")
+
+        wavfilepath = os.path.splitext(filepath)[0] + '.wav'
+        subprocess.call(['ffmpeg', '-i', filepath, wavfilepath])
+        result = aT.fileRegression(wavfilepath, "training/", "svm")
+
+        os.remove(filepath)
+        os.remove(wavfilepath)
         return jsonify(dict(zip(result[1], result[0])))
 
 
